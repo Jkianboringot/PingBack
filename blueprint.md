184c67a91f557379872abb0602f769497921cecb
Jkianboringot <slyrics443@gmail.com>
2025-08-22 14:13:40 +0800

    Start

---
+++ blueprint.md
@@ -0,0 +1,23 @@
+plan how you will create the app the feature,capabilities
+
+
+app function(how it look and work)
+look: basic fb style 
+work: also same as fb posting,creating user 
+
+must have:
+secure authentication, good error handling , easy and efficeint navigation, 
+also noh execuse code(code that work only under a certain condition),an mvc structure,
+use classes mostly and uses .env to store environment variale
+
+Database:
+
+
+
+feature: simple login,signup,posting and commeting 
+
+capabilities: an fb like app where user post or comment on to other user post and of course signup and login 
+
+
+
+make a simple one first then make it advance over time
---
+++ view/index.php
@@ -0,0 +1,23 @@
+plan how you will create the app the feature,capabilities
+
+
+app function(how it look and work)
+look: basic fb style 
+work: also same as fb posting,creating user 
+
+must have:
+secure authentication, good error handling , easy and efficeint navigation, 
+also noh execuse code(code that work only under a certain condition),an mvc structure,
+use classes mostly
+
+
+feature: simple login,signup,posting and commeting 
+
+capabilities: an fb like app where user post or comment on to other user post and of course signup and login 
+
+
+databse design:
+- user
+
+focus: 
+learn php so that you can be worthy of using laravel without or almost no hicup
